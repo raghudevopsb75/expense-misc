483f71c4cfeb8ea73cde19a787b3ddde39ebe67b
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-16 08:59:01 +0530

    Expense Scripts

--- jenkins-ip/main.tf
+++ jenkins-ip/main.tf
@@ -19,3 +19,16 @@ resource "aws_route53_record" "jenkins" {
   ttl = 10
 }
 
+
+data "aws_instance" "artifactory" {
+  instance_id = "i-0b61ad7b5b72ac2ac"
+}
+
+resource "aws_route53_record" "artifactory" {
+  name    = "artifactory.rdevopsb73.online"
+  type    = "A"
+  zone_id = "Z09059901XRPHNYMGLMJ4"
+  records = [data.aws_instance.artifactory.public_ip]
+  ttl = 10
+}
+
