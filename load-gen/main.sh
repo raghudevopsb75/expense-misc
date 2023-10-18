e188a2901e543d8982450bef9725b591e071d44e
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-18 08:34:13 +0530

    Expense Scripts

--- load-gen/main.sh
+++ load-gen/main.sh
@@ -1,7 +1,8 @@
 requests=$1
+url=$2
 
 curl -s "https://random-word-api.herokuapp.com/word?number=$requests" | sed -e "s/\[//" -e 's/\]//' | sed -e 's/,/ /g' -e 's/"//g' >/tmp/words
 
 for word in `cat /tmp/words`; do
-  echo $word
+  curl -X POST $url/api/transaction --header "Content-Type: application/json" -d "{\"amount\":\"$RANDOM\",\"desc\":\"$word\"}"
 done
